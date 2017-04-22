1f253d7a93749eaaa84f45dd7b162899e9a62d97
Nicholas Kridler <nmkridler@gmail.com>
2017-04-22 08:56:44 -0700

    updated run.py to use a token

--- run.py
+++ run.py
@@ -2,20 +2,36 @@ from sirbarksalot.lib.scheduler import Scheduler
 from sirbarksalot.listener.listen import Listener
 from sirbarksalot.listener.specgram import create_spectrogram, write_to_png
 from sirbarksalot.detector.match import MatchTemplate
+from sirbarksalot.messenger.facebook import URL, create_app
+import os
+PHONE_NUMBER = os.environ["PHONE_NUMBER"]
+
 
 import cv2
 import time
 def make_filename(ext, ms):
     return "./clips/sample_{0}.{1}".format(ms, ext)
 
+import requests
 import logging
 logging.basicConfig()
 logger = logging.getLogger("sirbarksalot")
 logger.setLevel('INFO')
 
 _MT = MatchTemplate(filename="./sirbarksalot/listener/test.wav")
+
 _LISTEN = Listener()
 _LISTEN.start_stream()
+def send_message(corr):
+    payload = {
+        'recipient': {'phone_number': PHONE_NUMBER},
+        'message': {'text': 'woof woof: {:.4f}'.format(corr)}
+    }
+
+    headers = {'Content-Type': 'application/json'}
+    print payload
+    r = requests.post(URL, json=payload, headers=headers)
+    print r
 
 def listen():
 
@@ -31,13 +47,18 @@ def listen():
         logger.info("Bark Found! Correlation: {}".format(score))
         _ms = "{0}".format(time.time()).split(".")[0]
         _LISTEN.write_to_file(data, make_filename("wav", _ms))
-        write_to_png(_specgram, make_filename("png", _ms))
+        # write_to_png(_specgram, make_filename("png", _ms))
+        send_message(score)
+        return 1
+    return 0
 
+
+app = create_app()
 
 if __name__ == "__main__":
     scheduler = Scheduler(6, listen)
     scheduler.start()
-    raw_input("Press Enter to Quit...")
+    app.run(debug=True, port=5555)
     scheduler.stop()
     _LISTEN.stop_stream()
     _LISTEN.shutdown()
