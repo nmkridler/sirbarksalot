22a953da9d82982cd688e125f33a63f27d704515
Nicholas Kridler <nmkridler@gmail.com>
2017-07-01 07:02:39 -0700

    updated training script to use 300 epochs

--- train.py
+++ train.py
@@ -1,15 +1,21 @@
 
-from sirbarksalot.cnn.labels import get_spectrograms, get_labels
+from sirbarksalot.cnn.labels import *
 from sirbarksalot.cnn.train import pipeline
+
+
+
 def main():
     df = get_labels("./labeled_clips/labels.csv")
 
+    # X, y = get_corr_features(df, basedir="./labeled_clips/")
     X, y = get_spectrograms(df, basedir="./labeled_clips/")
     print X.shape, y.shape
 
     params = {
-        "batch_size": 16,
-        "epochs": 200
+        "batch_size": 128,
+        "epochs": 300,
+        "save": True,
+        "kfold": False
     }
     pipeline(X, y, **params)
 
